54a2a0e57305a47c113116e3b27e5f58db43a8f2
JesusLugo2002 <jesusdlugo02@gmail.com>
2024-10-15 16:05:56 +0100

    Se terminan métodos de las bases de datos

--- js/models/database.js
+++ js/models/database.js
@@ -13,18 +13,47 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 
 // Connect with Firebase database
-const db = getFirestore(app)
+const db = getFirestore(app);
 
-class DBConnection {
+export default class DBConnection {
     constructor() {
-        this.usersCollection = collection(db, "pokemon");
+        this.usersCollection = collection(db, "pokemon"); // Users collection connection
     }
 
+    // Add rows to users collection
     async addFile(data) {
         try {
-            const docRef = await addDoc
+            const docRef = await addDoc(this.usersCollection, data);
+            console.log("Documento escrito con ID: ", docRef.id);
+            return docRef.id;
         } catch (e) {
             console.error("Error añadiendo registro: ", e)
+        }
+    }
+
+    // Read all rows from users collection
+    async readAll() {
+        try {
+            const querySnapshot = await getDocs(this.collectionRef);
+            const dataList = querySnapshot.docs.map((doc) => ({
+              id: doc.id,
+              ...doc.data(),
+            }));
+            console.log("Documentos:", dataList);
+            return dataList;
+        } catch (e) {
+            console.error("Error obteniendo documentos: ", e)
+        }
+    }
+
+    // Update row from users collection with ID
+    async update(id, data) {
+        try {
+          const docRef = doc(this.usersCollection, id);
+          await updateDoc(docRef, data);
+          console.log("Documento actualizado con ID: ", id);
+        } catch (e) {
+          console.error("Error actualizando documento: ", e);
         }
     }
 }
