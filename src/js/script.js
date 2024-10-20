1d7d7ebfba787d8c73907290cc5c3c905f6c9f69
JesusLugo2002 <jesusdlugo02@gmail.com>
2024-10-20 13:56:47 +0100

    Se añade funcionalidad de registrar nuevos usuarios

--- src/js/script.js
+++ src/js/script.js
@@ -22,9 +22,34 @@ async function login() {
     }
 }
 
-
 async function register() {
-    console.log("pipipi")
+    const inputUsername = document.getElementById("signup-username").value
+    const inputPassword = document.getElementById("signup-password").value
+    const repeatPassword = document.getElementById("signup-r-password").value
+
+    const allUsers = await database.readAll()
+
+    for (const user of allUsers) {
+        if (user["username"] == inputUsername) {
+            alert("Error: Username already used! Please use another username...")
+            return
+        }
+    } 
+
+    if (inputPassword == repeatPassword) {
+        const data = {
+            username: inputUsername,
+            password: inputPassword,
+            balance: 10000,
+            wishlist: [],
+            inventory: [],
+            shoppingCart: []
+        }
+        await database.addFile(data)
+        alert(`New user ${inputUsername} created! Please login into the shop...`)
+    } else {
+        alert("Password don't matches!")
+    }
 }
 
 signupButton.addEventListener("click", function(e) {
